e26510607f8fbaf02aa1372fd90caad2f8847835
Kristen Doucette <cosmicnoir90@gmail.com>
2019-06-06 18:26:31 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+function getParks() {
+// obtains videos with fectch
+}
+
+function handleForm() {
+// handles form data when submit button is clicked
+}
+
+function displayParks() {
+// displays video results in js-results ul
+}
+
+handleForm();
